7cf8e2c40e30c33434a542ed4f882b6dca6747ee
Alan Parker Lue <alan.parkerlue@gmail.com>
2017-12-21 13:36:04 -0800

    Cache data fetches

--- app/instrumdat/models.py
+++ app/instrumdat/models.py
@@ -1,19 +1,55 @@
+from cachetools import cached
+import datetime
 from flask import current_app
+import logging
 import pandas as pd
 import requests
 
 
-def fetch_instr(ticker):
+class InstrumdatKey:
+    def __init__(self, ticker, date=None):
+        self.ticker = ticker
+        self.date = date if date is not None else datetime.date.today()
+
+    def __eq__(self, other):
+        return self.ticker == other.ticker \
+            and self.date == other.date
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __str__(self):
+        return '{}: {}'.format(self.date, self.ticker)
+
+
+@cached(cache={})
+def _fetch_instr(instrumdat_key):
+    ticker = instrumdat_key.ticker
+    if instrumdat_key.date != datetime.date.today():
+        raise ValueError(
+            'cannot fetch data as of {}'.format(instrumdat_key.date)
+        )
+
     apikey = current_app.config['instrumdat']['apikey']
     url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json'
+
+    logging.info("Fetching data for ticker `{}'".format(ticker))
     r = requests.get(url, params={
         'ticker': ticker,
         'api_key': apikey,
-        'date': '1999-11-18,1999-11-19',
     })
     jdata = r.json()
     df = pd.DataFrame(
         jdata['datatable']['data'],
         columns=[x['name'] for x in jdata['datatable']['columns']]
     )
+
+    return df
+
+
+def fetch_instr(ticker):
+    """Fetch historical data for a financial instrument."""
+    instrumdat_key = InstrumdatKey(ticker)
+    df = _fetch_instr(instrumdat_key)
+
     return df
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import logging
 from app import app
 
+logging.getLogger().setLevel(logging.INFO)
 app.run(port=33507, debug=True)
