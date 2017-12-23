Correct moving average calculation

app/instrumdat/models.py
@@ -51,6 +51,8 @@ def _fetch_instr(instrumdat_key):
         jdata['datatable']['data'],
         columns=[x['name'] for x in jdata['datatable']['columns']]
     )
+    if df.empty:
+        raise ValueError("ticker `{}' returned no data".format(ticker))
 
     return df
 
@@ -65,15 +67,25 @@ def fetch_instr(ticker):
     return df
 
 
-def build_plot(ticker, instrument_variables):
-    df = fetch_instr(ticker)
-    n = min(len(df), 252)
+def moving_average(x, window, fill=True):
+    """Return moving average."""
+    if len(x) < window:
+        s = 'series length {} is smaller than window({})'
+        raise ValueError(s.format(len(x), window))
+    w = np.ones(window) / float(window)
+    a = np.convolve(x, w, 'valid')
+    if fill:
+        a = np.concatenate((np.nan * np.ones(window - 1), a))
+    return a
+
+
+def build_plot(df, instrument_variables, maxdays=252):
+    n = min(len(df), maxdays)
     df = df[-n:]
 
     # prepare some data
     dates = np.array(df['date'].values, dtype=np.datetime64)
-    window_size = 21
-    window = np.ones(window_size) / float(window_size)
+    window = 21
 
     # create a new plot with a a datetime axis type
     p = figure(width=800, height=350, x_axis_type="datetime")
@@ -82,26 +94,26 @@ def build_plot(ticker, instrument_variables):
     if 'close' in instrument_variables:
         p.circle(dates, df['close'],
                  size=4, color='darkgrey', alpha=0.2, legend='close')
-        avg = np.convolve(df['close'], window, 'same')
+        avg = moving_average(df['close'], window)
         p.line(dates, avg, color='navy', legend='avg close')
     if 'adj_close' in instrument_variables:
         p.circle(dates, df['adj_close'],
                  size=4, color='darkgreen', alpha=0.2, legend='adj_close')
-        avg = np.convolve(df['adj_close'], window, 'same')
+        avg = moving_average(df['adj_close'], window)
         p.line(dates, avg, color='green', legend='avg adj_close')
     if 'open' in instrument_variables:
         p.circle(dates, df['open'],
                  size=4, color='darkred', alpha=0.2, legend='open')
-        avg = np.convolve(df['open'], window, 'same')
+        avg = moving_average(df['open'], window)
         p.line(dates, avg, color='red', legend='avg open')
     if 'adj_open' in instrument_variables:
         p.circle(dates, df['adj_open'],
                  size=4, color='orange', alpha=0.2, legend='adj_open')
-        avg = np.convolve(df['adj_open'], window, 'same')
+        avg = moving_average(df['adj_open'], window)
         p.line(dates, avg, color='brown', legend='avg adj_open')
 
     # customize by setting attributes
-    p.title.text = "One-month average"
+    p.title.text = "One-month moving average"
     p.legend.location = "top_left"
     p.grid.grid_line_alpha = 0
     p.xaxis.axis_label = 'Date'